8e53b09a79521baaff5018e7815678717ebdd37e
Chevy Randani <chevy.warriorsmesmew@gmail.com>
2022-12-26 16:11:48 +0700

    Landing Page Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    screens: {
+      'sm': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1440px',
+      // => @media (min-width: 1440px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins',
+          'sans-serif'
+        ],
+        Montserrat: ['Montserrat', 'sans-serif'],
+        Pacifico: ['Pacifico', 'cursive'],
+        Michroma: ['Michroma', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
